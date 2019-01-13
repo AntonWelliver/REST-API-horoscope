305110c57c9f42c53e3a1911a75462c9678ed9a8
Anton <antonwelliver27@gmail.com>
2019-01-14 00:43:49 +0100

    commit

---
+++ addHoroscope.php
@@ -0,0 +1,9 @@
+<?php
+
+    header('Access-Control-Allow-Origin : *');
+    header('Content-Type: application/x-www-form-urlencoded');
+    header('Access-Control-Allow-Methods: POST');
+    
+    echo "addFunction";
+
+?>
