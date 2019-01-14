fc91f2cfac873295930852efcbaaf6dea7e3a0fc
Anton <antonwelliver27@gmail.com>
2019-01-14 19:50:00 +0100

    added functions in addHoroscope.php

--- addHoroscope.php
+++ addHoroscope.php
@@ -3,7 +3,35 @@
     header('Access-Control-Allow-Origin : *');
     header('Content-Type: application/x-www-form-urlencoded');
     header('Access-Control-Allow-Methods: POST');
-    
-    echo "addFunction";
 
+    $okToAdd = true;
+
+    //Check for POST function    
+    if($_SERVER['REQUEST_METHOD'] != 'POST') {
+        $okToAdd = false;
+        //Die??
+    }
+
+    //Check birthdate
+    $birthdate = "";
+    if($okToAdd && isset($_POST['birthdate'])) {
+        $birthdate = $_POST['birthdate'];
+        if(checkdate(birthdate) == false) {
+            $okToAdd = false;
+        }
+    }
+
+    //Check that $_SESSION is empty
+    if(isset($_SESSION['horoscope'])) {
+        $okToAdd = false;
+    }
+
+    //Add horoscope to session
+    if($okToAdd) {
+        $_SESSION['horoscope'] = getHoroscope($birthdate);
+        echo "true";
+    }
+    else {
+        echo "false";
+    }
 ?>
