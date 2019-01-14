557e6172241126e0a0c6b19a527611608851fb95
Anton <antonwelliver27@gmail.com>
2019-01-14 19:54:45 +0100

    commit

--- deleteHoroscope.php
+++ deleteHoroscope.php
@@ -1,7 +1,7 @@
 <?php
 
 //Check DELETE function
-    if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if($_SERVER['REQUEST_METHOD'] == 'DELETE') {
         session_start();
         if(isset($_SESSION['horoscope'])) {
             unset($_SESSION['horoscope']);
